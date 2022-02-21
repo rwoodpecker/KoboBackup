3aa1d03586a5b53f3c01e537ff03cf42f69726be
rwoodpecker <rwoodpecker@users.noreply.github.com>
2022-02-21 13:38:01 +1100

    improve exception handling, remove some comments.

--- kobo_backup.py
+++ kobo_backup.py
@@ -9,18 +9,17 @@ import shutil
 label = 'KOBOeReader' # name of kobo - this is the default.
 backup_base_directory = str(os.path.join(os.path.expanduser('~'), 'Backups', 'kobo')) # the folder in which backups will be placed. This should be os agnostic.
 
-print(backup_base_directory)
-
-try: #get kobo mount point
+try: # get kobo mount point on linux
     lsblk_check = subprocess.check_output(['lsblk', '-f', '--json']).decode('utf8')
     lsblk_json = json.loads(lsblk_check)
     kobos = [device for device in lsblk_json['blockdevices'] if device.get('label', None) == label]
     kobos = [kobo['mountpoint'] for kobo in kobos]
+    user_os = 'Linux'
 except FileNotFoundError:  # macOS (does not have lsblk)
-    print('lsblk not found - macOS detected.')
     df_output = subprocess.check_output(('df', '-Hl')).decode('utf8')
     output_parts = [o.split() for o in df_output.split('\n')]
     kobos = [o[-1] for o in output_parts if f'/Volumes/{label}' in o]
+    user_os = 'macOS'
 
 if len(kobos) > 1:
     raise RuntimeError(f'Multiple Kobo devices detected: {kobos}')
@@ -29,7 +28,7 @@ elif len(kobos) == 0:
     sys.exit()
 else:
     [kobo] = kobos
-    print(f'Kobo mountpoint is: {Path(kobo)}.')
+    print(f'Kobo mountpoint is: {Path(kobo)} on {user_os}.')
 
 backup_folder_exists = os.path.isdir(backup_base_directory) # check backup base directory exists locally, if not create it.
 if not backup_folder_exists:
@@ -40,14 +39,13 @@ else:
     print(f'An existing kobo backup folder was detected at {backup_base_directory}.')
 
 backup_path = os.path.join(backup_base_directory, 'kobo_backup_' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M'))
-print(backup_path)
 if os.path.isdir(backup_path):
-    print(f"A backup of the kobo has already completed at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}. Try again in a minute.")
+    print(f"A backup of the kobo was already completed at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}. Try again in a minute.")
     sys.exit()
 
 try: # copy files
     shutil.copytree(Path(kobo), os.path.join(backup_path))
-except:
+except OSError: # some unrequired .Trashes will return 'operation not permitted'.
     pass
 
 def get_directory_size(directory): # figure out how much was backed up
@@ -59,20 +57,12 @@ def get_directory_size(directory): # figure out how much was backed up
             elif entry.is_dir():
                 total += get_directory_size(entry.path)
     except NotADirectoryError:
-        # if `directory` isn't a directory, get the file size then
         return os.path.getsize(directory)
     except PermissionError:
-        # if we can't open the folder, return 0
         return 0
     return total
 
 def get_size_format(b, factor=1024, suffix="B"): # convert bytes to something human readable.
-    """
-    Scale bytes to its proper byte format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
     for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
         if b < factor:
             return f"{b:.2f}{unit}{suffix}"
