d6b4c2a17501a3c35e22486dc431c86029abfbd7
whatageareyouin <ellis.xanthis@gmail.com>
2022-03-05 16:49:16 +0800

    script now correctly reports which os is detected

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import platform
 import subprocess
 from typing import List
 
+
 def get_directory_size(directory):  # figure out how much was backed up.
     total = 0
     try:
@@ -28,6 +29,7 @@ def get_size_format(
             return f"{b:.2f}{unit}{suffix}"
         b /= factor
     return f"{b:.2f}Y{suffix}"
+
 
 def get_user_os_and_kobo_mountpoint(label):
     """
@@ -54,12 +56,12 @@ def get_user_os_and_kobo_mountpoint(label):
             if device.get("label", None) == label
         ]
         kobos = [kobo["mountpoint"] for kobo in kobos]
-        return UserSystemInfo(user_os="Windows", kobos=kobos)
+        return UserSystemInfo(user_os="Linux", kobos=kobos)
     elif platform.system() == "Darwin":  # Get mount point on MacOS
         df_output = subprocess.check_output(("df", "-Hl")).decode("utf8")
         output_parts = [o.split() for o in df_output.split("\n")]
         kobos = [o[-1] for o in output_parts if f"/Volumes/{label}" in o]
-        return UserSystemInfo(user_os="Windows", kobos=kobos)
+        return UserSystemInfo(user_os="macOS", kobos=kobos)
     else:
         raise Exception(f"Unsupported OS: {platform.system()=} {platform.release()=}")
 
@@ -70,7 +72,6 @@ class UserSystemInfo:
     Class to hold user system information.
     Has two attributes: user_os (str) and kobos (list of str of paths to mounted Kobo devices).
     """
+
     user_os: str
     kobos: List[str]
-    
-
